aa5636f429c071a802a847f0d4595f67a8fea049
Shinetopia <shinetopiaofficial@outlook.com>
2025-09-25 11:51:28 +0800

    Reading PSD voltage

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,27 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
 
-#define BTN_LEFT 0
-#define BTN_RIGHT 14 
+#define PSD_PIN 12
 
 TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
   Serial.begin(9600);
+  pinMode(PSD_PIN, INPUT);
 
   tft.init();
   tft.setRotation(3);
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
   tft.setTextSize(3);
-  tft.setCursor(10, 50);
-  tft.print("Hello, world");
 }
 
 void loop() {
-
+    int reading = analogRead(PSD_PIN);
+    float percentage = (float)reading / 4095.0f;
+    float scaledVal = percentage * 3.3f;
+    tft.fillScreen(TFT_BLACK);
+    tft.setCursor(10, 50);
+    tft.printf("PSD voltage: %0.2f", scaledVal);
+    delay(500);
 }
